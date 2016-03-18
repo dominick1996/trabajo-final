9f1b7e0c3491b49f274fa5b96056eee46b5cf62c
david carbajal criollo <riccc.jjjhhggg@gmail.com>
2016-03-18 13:50:58 -0500

    examen

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title></title>
@@ -8,7 +9,8 @@
 	<link rel="stylesheet" href="public/css/estilos.css">
 
 </head>
-<body background="public/slider/4.jpg" bgcolor="white" text="white">
+<body background="public/slider/4.jpg" bgcolor="white" text="white" style=text-align: "justify"> 
+
 <div class="container">
 	<nav class="navbar navbar-default" role="navigation">
 		<div class="container-fluid">
@@ -20,14 +22,14 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">ResIDEnT EViL</a>
+				<a class="navbar-brand" href="#">MACHU PICCHU</a>
 			</div>
 	
 			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse navbar-ex1-collapse">
 				<ul class="nav navbar-nav">
-					<li class="active"><a href="#">miedo</a></li>
-					<li><a href="#">terror</a></li>
+					<li class="active"><a href="#">HISTORIA</a></li>
+					<li><a href="#">COSTUMBRES</a></li>
 				</ul>
 				<form class="navbar-form navbar-left" role="search">
 					<div class="form-group">
@@ -36,14 +38,14 @@
 					<button type="submit" class="btn btn-default">buscar</button>
 				</form>
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="#">suspenso</a></li>
+					<li><a href="#">RELIGIÓN</a></li>
 					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Generos<b class="caret"></b></a>
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">COMIDAS<b class="caret"></b></a>
 						<ul class="dropdown-menu">
-							<li><a href="#">espelusnante</a></li>
-							<li><a href="#">real</a></li>
-							<li><a href="#">macabros</a></li>
-							<li><a href="#">para no dormir</a></li>
+							<li><a href="#">chicharon de  chancho</a></li>
+							<li><a href="#">cuy chactado</a></li>
+							<li><a href="#">chiriucho</a></li>
+							<li><a href="#">olluquito con charqui</a></li>
 						</ul>
 					</li>
 				</ul>
@@ -51,7 +53,7 @@
 		</div>
 	</nav>
 </div>
-
+<center><table border="8" width="95%" background="public/slider/4.jpg">
 
 <div id="carousel-id" class="carousel slide" data-ride="carousel">
 	<ol class="carousel-indicators">
@@ -64,7 +66,7 @@
 			<img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" alt="First slide" src="public/slider/1.jpg">
 			<div class="container">
 				<div class="carousel-caption">
-					<h1>si la vida fuera esto.</h1>
+					<h2>La maravilla el Perú.</h2>
 					<p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
 					<p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
 				</div>
@@ -74,7 +76,7 @@
 			<img data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide" alt="Second slide" src="public/slider/2.jpg">
 			<div class="container">
 				<div class="carousel-caption">
-					<h1>que es lo que harias tu.</h1>
+					<h2>es  muy hermoso verdad.</h2>
 					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
 					<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
 				</div>
@@ -84,7 +86,7 @@
 			<img data-src="holder.js/900x500/auto/#555:#5a5a5a/text:Third slide" alt="Third slide" src="public/slider/3.jpg">
 			<div class="container">
 				<div class="carousel-caption">
-					<h1>te gustaria vivir en un mundo asi</h1>
+					<h2>te gustaria vivir aqui.</h2>
 					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
 					<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
 				</div>
@@ -110,14 +112,14 @@
 		<div class="col-md-4">
 			<div class="mi-video">
 				<video autobuffer autoloop loop controls>
-					<source src="public/video/2.mp4">
+					<source src="public/video/1.mp4">
 				</video>
 			</div>
 		</div>
 		<div class="col-md-4">
 			<div class="mi-video">
 				<video autobuffer autoloop loop controls>
-					<source src="public/video/3.mp4">
+					<source src="public/video/1.mp4">
 				</video>
 			</div>
 		</div>
@@ -128,21 +130,21 @@
 		<div class="col-md-4">
 			<div class="mi-video">
 				<video autobuffer autoloop loop controls>
-					<source src="public/video/4.mp4">
+					<source src="public/video/2.mp4">
 				</video>
 			</div>
 		</div>
 		<div class="col-md-4">
 			<div class="mi-video">
 				<video autobuffer autoloop loop controls>
-					<source src="public/video/5.mp4">
+					<source src="public/video/2.mp4">
 				</video>
 			</div>
 		</div>
 		<div class="col-md-4">
 			<div class="mi-video">
 				<video autobuffer autoloop loop controls>
-					<source src="public/video/6.mp4">
+					<source src="public/video/2.mp4">
 				</video>
 			</div>
 		</div>
@@ -150,20 +152,108 @@
 	</div>
 
 
-
-
-
-
-
-
 </div>
- 
+	<script src="public/js/jquery-1.12.1.js"></script>
+	<script src="public/js/bootstrap.js"></script>
+
+
+<tr>
+<tr>
+<td><center><h3><font color="blue">
+<center><h3><font color="orange" face="georfia">1. viajar sin limites:</p></h3><font color="skyblue"></center><h5  p align="left/center/right/justify">“Viajar es una brutalidad. Te obliga a confiar en extraños y a perder de vista todo lo que te resulta familiar y confortable de tus amigos y tu casa. Estás todo el tiempo en desequilibrio. Nada es tuyo excepto lo más esencial: el aire, las horas de descanso, los sueños, el mar, el cielo; todas aquellas cosas que tienden hacia lo eterno o hacia lo que imaginamos como tal”.
+Fin de la conversación
+
+.</h5>
+
+
+<div class="col-md-12">
+	<div class="col-md-4" background="public/slider/3.jpg">
+		
+		<div class="panel panel-info border-panel">
+			<div class="panel-heading">
+				<h2 class="panel-title"><font color="black" face="georfia"><strong>HISTORIA CULTURAL DEL PERÚ</h2></strong>
+			</div>
+			<div class="panel-body">
+				<img src="public/slider/5.jpg" height="300" width="370">
+			</div>
+			<p><h4><center><h3><font color="orange" face="georfia">1. SACSAYHUAMÁN:</p></h3><font color="skyblue"></center><h5 p align="left/center/right/justify">
+			
+Sacsayhuamán (en quechua Saqsaywaman, de saqsay, lugar de saciarse, y waman, halcón, es decir, "Lugar donde se sacia el halcón") es una "fortaleza ceremonial" inca ubicada dos kilómetros al norte de la ciudad del Cuzco. Se comenzó a construir durante el gobierno de Pachacútec, en el siglo XV; sin embargo, fue Huayna Cápac quien le dio el toque final en el siglo XVI. Con el aniquilamiento de la nobleza inca desaparecieron de la memoria humana las técnicas que permitieron la construcción de esta monumental fortaleza o santuario; el cual produjo la admiración de Pizarro y sus hombres. Pedro Sancho, secretario de Pizarro, deja una primera descripción del edificio; pero la más detallada es la del Inca Garcilaso de la Vega (Cronista mestizo). La "fortaleza ceremonial" de Sacsayhuamán es con sus muros megalíticos, la mayor obra arquitectónica que realizaron los incas durante su apogeo. Desde la fortaleza se observa una singular vista panorámica de los entornos, incluyendo la ciudad del Cuzco.La construcción es típica de la arquitectura inca, aunque es el lugar donde esta técnica se usó a mayor escala. La roca se cortaba en la cantera de forma tosca con la forma aproximada requerida. Luego era arrastrada con cuerdas por cientos de hombres hasta el lugar de construcción, donde se le daba el tallado definitivo. Las cuerdas usadas eran tan impresionantes que fueron mencionadas por el conquistador español Diego de Trujillo en 1571 tras inspeccionar una habitación llena de material de construcción.
+
+
+Una garra de puma hecha de piedras.
+
+Cisterna para retener agua de lluvia.
+
+Detalle de un muro.
+
+Puerta en el muro.
+La suavidad aterciopelada de la piedra resalta en estas formaciones. Hay figuras diseñadas en las piedras y rocas, entradas a túneles subterráneos o chicanas, anfiteatros, construcciones de carácter ritual, probablemente relacionadas con el culto al agua. Este sitio desempeñó un importante rol en las actividades rituales incaicas.Hubo tres torreones ubicados en la parte superior de la fortaleza: Muyucmarca, Sallacmarca y Paucamarca. El primero tenía una forma cilíndrica con una altura equivalente a unos cuatro pisos y un diámetro de unos 22 metros. Se encontraba en el centro. El segundo tuvo forma cuadrangular. Desde allí se divisaba toda la ciudad del Cuzco. El tercero, también de forma cuadrada, estaba ubicado al otro extremo del Sallacmarca.
+Fin de la conversación
+.</h5></p>
+		</div>
+		
+	</div>
 
 
 
 
+	<div class="col-md-4">
+		
+		<div class="panel panel-info border-panel">
+			<div class="panel-heading">
+				<h3 class="panel-title"><font color="black" face="georfia"><strong>HISTORIA CULTURAL DEL PERÚ</h3></strong>
+			</div>
+			<div class="panel-body">
+				<img src="public/slider/6.jpg" height="300" width="390">
+			</div>
+			<p><h4><center><h3><font color="orange" face="georfia" >2.MACHU PICCHU:</p></h3><font color="skyblue"></center><h5 p align="left/center/right/justify">
+			Machu Picchu es como se conoce hoy en día a esta ciudadela del Imperio Inca cuyas ruinas datan del 1300 DC. Sin embargo "Montaña Vieja", como su traducción indica, proviene del lenguaje nativo Quechua, lengua autóctona aún con mucha presencia en algunas comunidades del sur del país, donde esta ciudad es conocida como Machu Picchu. Esta ciudadela contiene aún los restos de la antiguo asentamiento formado por este poblado parte de la civilización inca, localizado al este de la depresión formada por los sistemas montañosos Machu Picchu y Huayna Picchu con los Andes Centrales al sur del país. Esta localización geográfica en la ladera este de la cordillera del Vilcanota a tan solo 80 kilómetros de la ciudad de Cuzco, aún posee muchas de los restos y ruinas de piedra construidas por todas las laderas de estos sistemas montañosos, presentando los diferentes períodos históricos de la civilización inca que dejaron sus huellas e historia en la ciudadela de Machu Picchu.
 
-	<<script src="public/js/jquery-1.12.1.js"></script>
-	<<script src="public/js/bootstrap.js"></script>
+La construcción de la ciudadela de Machu Picchu se atribuye al gran emperador, así como gran estadista inca, Pachacútec que, durante su imperio desde el 1438 al 1471 DC, planificó el diseño de esta ciudadela para atajar el problema del crecimiento demográfico de la población así como la explotación de las tierras. Esta situación demográfica forzó a Pachacútec a enviar a una serie de equipos de exploración para asegurar los asentamientos subsiguientes. Tras la victoria en la batalla contra la civilización chanca librada en el Machu Picchu, el imperio de Pachacútec empezó su expansión, llegando hasta su máxima culminación con la creación de la gran ciudad del Cuzco.
+
+Todo indica que el motivo principal fue tanto la expansión demográfica como intelectual, debido a que Pachacútec fue bien conocido como un líder espiritual y un excelente dirigente. El uso final de la ciudadela quedó reservado estrictamente para la alta nobleza inca y gracias a que fue estratégicamente diseñada para aprovechar su localización al máximo, la ciudadela supo cómo alojar a más de tres generaciones a la vez que evitaba el acceso a todo aquel que no perteneciese a la nobleza inca, pues solo existía una única entrada a la que fue utilizado a su favor para mejorar las medidas de protección de la ciudad. El asentamiento se encuentra localizado entre valles profundos rodeados de kilómetros de densa selva.
+
+. </h5></p>
+		</div>
+	</div>
+
+
+
+	<div class="col-md-4">
+		
+		<div class="panel panel-info border-panel">
+			<div class="panel-heading">
+				<h3 class="panel-title"><font color="black" face="georfia" ><strong>HISTORIA CULTURAL DEL PERÚ</h3></strong>
+			</div>
+			<div class="panel-body">
+				<img src="public/slider/7.jpg" height="300" width="370">
+			</div>
+			<p><h4><center witdh="400"><h3><font color="orange" face="georfia">3.CORICANCHA:</p></h3><font color="skyblue"></center><h5 p align="left/center/right/justify">
+
+		Fue uno de los más famosos, venerados y respetados templos de la ciudad del Cuzco, en el Perú.
+
+
+Ruinas actuales del templo del Sol o Coricancha, conservadas dentro del templo de Santo Domingo.
+El recinto de oro; como era conocido, era un lugar sagrado donde se rendía adoración al máximo dios inca: el Inti (Sol), por lo que sólo podían entrar en ayunas, descalzos y con una carga en la espalda en señal de humildad, según lo indicaba el sacerdote mayor Willaq Umu .
+
+El frontis era un hermoso muro proveniente de la más fina cantería, decorado únicamente por una fina lámina de oro puro de una palma de alto, a tres metros del suelo, y un techo de paja fina y delicadamente cortada.
+
+En uno de los bloques de la segunda hilada se observan tres agujeros que pudieron ser utilizados para evacuar las aguas de las lluvias del patio interior, o como salida de la chicha que se ofrecía como ofrenda. Según los experimentos de Augusto León Barandiarán, si se golpea dentro de los agujeros se pueden escuchar las notas musicales "re", "la" y "mi".
+
+Las piedras que componen el templo tienen un leve almohadillado en los lados, que expresan la sobria estética de la construcción en el Imperio inca. Antiguamente no existía el atrio triangular que sirve de entrada al templo colonial y el muro giraba en ángulo recto hacia la calle Ahuacpinta (Awaq Pinta), la cual aún conserva un tramo del muro original de casi sesenta metros de largo. En el lado opuesto a esta calle, el muro se hace curvo al girar más de 90 grados, y continúa con una curva suave que fue cortada durante la construcción del templo. El muro del Qurikancha coronaba un sistema de andenes que bajaban hasta el río.
+
+Muchos libros e historiadores señalan que este lugar fue más santificado o venerado, porque al construir un templo o convento de parte de los españoles, a este lugar lo pusieron como privilegiado para las ofrendas.1
+
+Dentro de los tres templos que aún existen se puede observar una parte de la pared que aún sigue con yeso y pintada, los españoles en su afán de que no se descubra las construcciones incas, cubrieron las paredes con yeso, pintaron e instalaron imágenes de santos y otras cosas para tapar todo rastro inca. Pero por causas naturales, como el terremoto que azotó el Cuzco las paredes se llegaron a cuartear y es así como se produce los hallazgos de las construcciones incas que hoy se exhiben dentro de la iglesia. 
+  </h5></p>
+  
+		
+		</div>
+	</div>
+</div>
+
+
+<p><marquee bgcolor="black" direction=left>**creado por: David. Vanessa. Naida**</marquee></p>
+
 </body>
-</html>
